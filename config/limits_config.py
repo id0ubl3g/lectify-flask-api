0f8289510e5d8b7ccd35f05603abd07ae2c283f8
id0ubl3g <id0ubl3g@gmail.com>
2026-09-20 11:23:48 -0300

    feat: add plan quotas and usage tracking

--- config/limits_config.py
+++ config/limits_config.py
@@ -1,1 +1,13 @@
 ABUSE_LIMIT = "5 per minute"
+
+PLAN_LIMITS = {
+    "1_month": "1 per minute;12 per 7 days;50 per month",
+    "6_months": "1 per minute;24 per 7 days;100 per month",
+    "1_year": "5 per minute;50 per 7 days;210 per month"
+}
+
+PLAN_RANKS = {
+    "1_month": 1,
+    "6_months": 2,
+    "1_year": 3
+}
--- config/providers/initialize_mercadopago.py
+++ config/providers/initialize_mercadopago.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from datetime import timedelta
 import mercadopago
 import os
 
@@ -7,29 +6,29 @@ load_dotenv()
 
 REVOKING_STATUSES = frozenset({"refunded", "charged_back", "cancelled"})
 
+PLANS = {
+    "1_month": {
+        "name": "1 Month",
+        "price": 19.90,
+        "days": 30
+    },
+    "6_months": {
+        "name": "6 Months",
+        "price": 99.90,
+        "days": 180
+    },
+    "1_year": {
+        "name": "1 Year",
+        "price": 179.90,
+        "days": 365
+    }
+}
+
 def initialize_mercadopago() -> dict:
     sdk = mercadopago.SDK(os.getenv("MERCADOPAGO_SECRET_KEY"))
-
-    plans = {
-        "1_month": {
-            "name": "1 Month",
-            "price": 19.90,
-            "days": 30
-        },
-        "6_months": {
-            "name": "6 Months",
-            "price": 99.90,
-            "days": 180
-        },
-        "1_year": {
-            "name": "1 Year",
-            "price": 179.90,
-            "days": 365
-        }
-    }
 
     return {
         "mercadopago": sdk,
         "webhook_secret": os.getenv("MERCADOPAGO_WEBHOOK_SECRET"),
-        "plans": plans,
+        "plans": PLANS,
     }
