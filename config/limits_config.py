8a801fd775460910e0d795807e4fb76495fc3d13
id0ubl3g <id0ubl3g@gmail.com>
2026-09-20 19:12:07 -0300

    chore: rebalance plan quotas and rate limits

--- config/limits_config.py
+++ config/limits_config.py
@@ -1,8 +1,8 @@
-ABUSE_LIMIT = "5 per minute"
+ABUSE_LIMIT = "15 per minute"
 
 PLAN_LIMITS = {
-    "1_month": "1 per minute;12 per 7 days;50 per month",
-    "6_months": "1 per minute;24 per 7 days;100 per month",
+    "1_month": "5 per minute;12 per 7 days;50 per month",
+    "6_months": "5 per minute;24 per 7 days;100 per month",
     "1_year": "5 per minute;50 per 7 days;210 per month"
 }
 
